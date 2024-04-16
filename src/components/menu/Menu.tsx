2e3dc2a596b7e2e17869e3fd2b916121f4118a1f
EagleBlood <35221@student.pwsztar.edu.pl>
2024-04-16 15:16:31 +0200

    navigation done

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,28 +1,30 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import { MenuProps } from "./menu.props";
 import { Wrapper } from './menu.style';
 import { PhUserBold } from '../../assets/PhUserBold';
 import { PhHome } from '../../assets/PhHome';
+import { Link, useLocation } from 'react-router-dom';
 
 const Menu: FunctionComponent<MenuProps> = ({}) => {
-  const [selectedItem, setSelectedItem] = useState('Dashboard');
+  const location = useLocation();
+  const [selectedItem, setSelectedItem] = useState(location.pathname);
+
+  useEffect(() => {
+    setSelectedItem(location.pathname);
+  }, [location]);
 
   return (
     <Wrapper>
       <div className="menuContainer">
-        <div className={selectedItem === 'Dashboard' ? "itemSelected" : "item"}
-          onClick={() => setSelectedItem('Dashboard')}
-        >
+        <Link to='/home' className={selectedItem === '/home' ? "itemSelected" : "item"}>
           <PhHome/>
           <p>Dashboard</p>
-        </div>
+        </Link>
 
-        <div className={selectedItem === 'Users' ? "itemSelected" : "item"}
-          onClick={() => setSelectedItem('Users')}
-        >
+        <Link to='/profile' className={selectedItem === '/profile' ? "itemSelected" : "item"}>
           <PhUserBold/>
           <p>Users</p>
-        </div>
+        </Link>
         <p className="appName"><span>C - Core</span></p> {/*Span for styling first letter*/}
         <p className="verName"><span>v 0.43.5</span></p>
       </div>
