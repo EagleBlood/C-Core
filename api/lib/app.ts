67de7fa7d0c4df8c767cd5e979669e650283f1bc
EagleBlood <35221@student.pwsztar.edu.pl>
2024-05-06 15:50:07 +0200

    base localhost site

--- api/lib/app.ts
+++ api/lib/app.ts
@@ -1,17 +1,25 @@
 import express from 'express';
-import { config } from './config'
+import { config } from './config';
+import Controller from "./interfaces/controller.interface";
 
 class App {
     public app: express.Application;
- 
-    constructor() {
-        this.app = express();
-    }
- 
-    public listen(): void {
-        this.app.listen(config.port, () => {
-            console.log(`App listening on the port ${config.port}`);
-        });
-    }
- }
- export default App;
+
+   constructor(controllers: Controller[]) {
+       this.app = express();
+       this.initializeControllers(controllers);
+   }
+
+   private initializeControllers(controllers: Controller[]): void {
+       controllers.forEach((controller) => {
+           this.app.use('/', controller.router);
+       });
+   }
+
+   public listen(): void {
+       this.app.listen(config.port, () => {
+           console.log(`App listening on the port ${config.port}`);
+       });
+   }
+}
+export default App;
